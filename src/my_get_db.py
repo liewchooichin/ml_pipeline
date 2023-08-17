7fa0bfc4b741f8c540bf9912dcd19e189f067c6e
liew <127520432+liewchooichin@users.noreply.github.com>
2023-08-17 21:30:52 +0800

    low level workhorse for the higher level get_score_db

---
+++ src/my_get_db.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 30 13:18:39 2023
+
+@author: Liew
+
+Reading sqlite3 database file from a url,
+write the database bytes to a local file,
+then use sqlite3 to read the file  
+and return a dataframe
+"""
+import pandas as pd
+import requests
+import sqlite3
+import joblib
+import os
+
+
+def db_pickle(db_url, table_name, local_db_name=None, pickle_name=None):
+    """
+    Read sqlite3 db from the url. Then, store it to a pickle file.
+
+    Parameters
+    ----------
+    db_url : str
+        The url of the sqlite3 db.
+    table_name : str
+        The name of the database table.
+    local_db_name : str
+        Local filename to store the db in the url.
+    pickle_name : str
+        Local filename of the dataframe pickle.
+
+    Returns
+    -------
+    Status of execution.
+
+    """
+
+    # Default name of the dataframe pickle
+    if pickle_name is None:
+        pickle_name = table_name + '_df.pkl'
+
+    # Read from the url
+    df_data = read_sqlite3_from_url(db_url, table_name, local_db_name)
+
+    # Save the dataframe to a pickle file
+    job_done = joblib.dump(df_data, pickle_name)
+    print(f"pickle at {job_done}")
+    if os.path.isfile(pickle_name):
+        return True
+    else:
+        return False
+
+
+def read_sqlite3_from_url(db_url, table_name, local_filename=None):
+    """
+    Read sqlite3 db file from a url and return a
+    dataframe.
+
+    Parameters
+    ----------
+    db_url : str
+        The url like https://---/some.db.
+    table_name : str
+        The name of the database table.
+    local_filename: str
+        Local filename to store the db in the url.
+
+    Returns
+    -------
+    dfa : pd.DataFrame
+        pd DataFrame of the db provided in the url.
+
+    """
+
+    # make a request to the url to get the database
+    r = requests.get(db_url)
+    r.raise_for_status()
+    print(f"Read the url of the database in {type(r)}.")
+    print(f"Response code is {r.status_code}.")
+
+    print(f"Length of content is {len(r.content)} \n"
+          f"of type {type(r.content)}")
+
+    # write the bytes to a local file,
+    # the bytes are specified with wb
+    # default local filename is table.db
+    if local_filename is None:
+        local_filename = table_name + '.db'
+
+    with open(local_filename, "wb") as f:
+        f.write(r.content)
+    f.close()
+
+    # connect the written file to sqlite3
+    conn = sqlite3.connect(local_filename)
+
+    # assign the connection to a pandas data frame
+    query = "SELECT * FROM " + table_name
+    dfa = pd.read_sql(query, conn)
+
+    # close the database connection
+    conn.close()
+    # remove the temporary file
+    # os.remove(local_filename)
+
+    # return the database read
+    return dfa
