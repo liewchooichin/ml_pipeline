149c8bdad8d2fbca74e49a2a22d50e066d562fe4
liew <127520432+liewchooichin@users.noreply.github.com>
2023-08-17 21:17:42 +0800

    first to test if it is ok

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PATH=$PATH:/home/runner/work/aiap-bootcamp1-liew-chooi-chin/aiap-bootcamp1-liew-chooi-chin/src
+export PATH
+cd /home/runner/work/aiap-bootcamp1-liew-chooi-chin/aiap-bootcamp1-liew-chooi-chin/src
+python get_score_db.py
+python main_score.py
